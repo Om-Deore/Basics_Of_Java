3f777005470a3c42ccd313fadd750512295a9556
Om Deore <omdeore321@gmail.com>
2024-12-13 13:39:08 +0530

    Add files via upload

---
+++ First_Program.java
@@ -0,0 +1,25 @@
+public class First_Program {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+// 1. Functions = A block of code that performs a specific task and is contained within a class or interface
+// 2. Classes   = A class is a blueprint for creating objects. In Java, every program must be contained within a class.
+
+
+/* package = Used to group related class. Think of it as a folder in a file directory
+ * public  = Access modifier : Specifies that the class or method is accessible from anywhere in your program.
+ * static  = Keyword         : Makes it possible to run the program without calling the function
+ * void    = Keyword         : It means that it doesn't give anything in return
+ * main    = Method name     : Name of function
+ * system  = Class           : A built-in Java class that provides access to system resources.
+ * out     = Object          : A static member of the System class. Represents the standard output stream (usually the console).
+ * println = Method          : Prints a line of text to the output and moves the cursor to a new line.
+ */
+
+/*
+#Naming Conventions
+    1. For Classes, we use PascalConvention
+    2. For Functions, we use camelCaseConvention
+*/
